7ab4d207f299599894d16c63b0ed2ca082364212
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2025-01-25 17:34:06 -0300

    refactor db.py to separate engine from table creation

--- src/db.py
+++ src/db.py
@@ -5,21 +5,18 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from models.patient import Patient
 
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+DB_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
+
+engine = create_engine(DB_URL, echo=True)
+
 def create_database_and_tables():
-    MYSQL_HOST = os.getenv("MYSQL_HOST")
-    MYSQL_PORT = os.getenv("MYSQL_PORT")
-    MYSQL_USER = os.getenv("MYSQL_USER")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
-
-    DB_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
-
-    engine = create_engine(DB_URL, echo=True)
-
     try:
         SQLModel.metadata.create_all(bind=engine)
         print("Tables created successfully.")
     except SQLAlchemyError as e:
         print(f"Error creating tables: {e}")
-
-    return engine
