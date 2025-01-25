5cf0c3d97f4c41f76c18e1dfabe5b11766750383
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2025-01-25 20:02:41 -0300

    fix patient phone number validation

--- src/models/patient.py
+++ src/models/patient.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel, Field
-from pydantic import EmailStr
+from pydantic import EmailStr, field_validator
 import phonenumbers
 
 class PatientBase(SQLModel):
@@ -7,17 +7,16 @@ class PatientBase(SQLModel):
     email: EmailStr
     phone_number: str
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.validate_phone_number()
-
-    def validate_phone_number(self):
+    @field_validator("phone_number")
+    @classmethod
+    def validate_phone_number(cls, value):
         try:
-            parsed_phone = phonenumbers.parse(self.phone_number)
+            parsed_phone = phonenumbers.parse(value)
             if not phonenumbers.is_valid_number(parsed_phone):
                 raise ValueError("Invalid phone number")
         except phonenumbers.phonenumberutil.NumberParseException:
             raise ValueError("Invalid phone number format")
+        return value
 
 class Patient(PatientBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
